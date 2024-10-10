69d19fe066f1b8d42e175c015d36e289625188de
Li Zhiwei <15128472546@163.com>
2024-10-10 15:43:14 +0800

    删除hello.R

---
+++ R/fangbian-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr mutate
+#' @importFrom glue glue
+#' @importFrom logger log_info
+## usethis namespace: end
+NULL
